416029e3aef7c11c28820164c5d70cd1afeb191e
franco2 <franco@DESKTOP-QNPNQF8>
2022-02-27 21:07:51 +0100

    nueva rama

--- Practica 5-1 entornos de desarrollo/Practica 5-1 entornos de desarrollo/Form1.Designer.cs
+++ Practica 5-1 entornos de desarrollo/Practica 5-1 entornos de desarrollo/Form1.Designer.cs
@@ -85,9 +85,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(533, 107);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.Size = new System.Drawing.Size(33, 13);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Nota";
+            this.label2.Text = "Nota:";
             // 
             // label3
             // 
